9b7266ebbe2dae6e2ed365bf043452a37ec6694c
Ibbske <ialshabee@gmail.com>
2026-04-22 10:29:21 +0200

    update styles and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/mindpark-glow-up/",
+  base: "/mindpark-glow-up",
   server: {
     host: "::",
     port: 8080,
